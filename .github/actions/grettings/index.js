5f5a9b8b9c6f2bbda8de21f11a9e8afb4154cef6
Loick21 <loicnzenang@gmail.com>
2026-06-18 09:59:03 +0000

    test

--- .github/actions/grettings/index.js
+++ .github/actions/grettings/index.js
@@ -1,12 +1,18 @@
-import { getInput, setOutput } from "@actions/core";
-import { GitHub } from "@actions/github";
+const core = require('@actions/core');
 
-function run() {
-    try{
-        const username = new GitHub(getInput("username", { required: true }))
-        const greeting = new GitHub(getInput("greeting", { required: true }))
-        setOutput("message",`${username} ${username}`)
-    }catch(err){
-        console.log(err)
-    }
+try {
+    // Lire les entrées fournies par l'utilisateur
+    const username = core.getInput('username');
+    const greeting = core.getInput('greeting');
+
+    // Construire le message
+    const message = `${greeting}, ${username}!`;
+
+    // Afficher le message dans les logs
+    console.log(message);
+
+    // Définir une sortie pour l'action
+    core.setOutput("message", message);
+} catch (error) {
+    core.setFailed(`Une erreur est survenue : ${error.message}`);
 }
